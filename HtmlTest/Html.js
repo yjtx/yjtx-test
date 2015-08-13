119c54a0697d415ecdfe977d5bef1fd16d2d0513
yjtx <friendhhy@163.com>
2015-08-13 20:30:52 +0800

    增加bug列表

--- HtmlTest/Html.js
+++ HtmlTest/Html.js
@@ -1,10 +1,14 @@
 var examples = {
-    "list": ["input", "textarea", "trans", "userAgent", "client", "save", "sound", "effect", "select"],
+    "list": ["input", "textarea", "trans", "userAgent", "client", "save", "sound", "effect", "select", "image", "imageD", "canvasImage"],
+    "浏览器bug列表": ["猎豹img跨域", "IOS微信Iframe输入框"],
     "": []
 };
 
 function returnHref(thirdName, forthName) {
-    return "HtmlTest/" + forthName + ".html" + "?r=" + Math.random();
+    if (thirdName == "list") {
+        return "HtmlTest/" + forthName + ".html" + "?r=" + Math.random();
+    }
+    return "HtmlTest/bugs/" + forthName + ".html" + "?r=" + Math.random();
 }
 
 createRoot("DOM", "htmls", examples, returnHref);
